9b1d30e8e0b5336177b125f2ab76213db12d889b
rateshsachdeva <rateshsachdeva@gmail.com>
2025-04-27 18:56:33 +0530

    Create ClientLayout.tsx

---
+++ app/ClientLayout.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { ReactNode } from 'react';
+
+export default function ClientLayout({ children }: { children: ReactNode }) {
+  // You can use hooks and other client-side features here
+  return <>{children}</>;
+}
