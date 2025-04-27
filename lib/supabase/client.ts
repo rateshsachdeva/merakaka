8ac7b318ab534251af12f41d7e1c8da4a3d83f19
rateshsachdeva <rateshsachdeva@gmail.com>
2025-04-27 17:07:00 +0530

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,7 +1,21 @@
-import { createBrowserClient } from "@supabase/ssr"
+import { createBrowserClient, createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
 
-export const createClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
+export const createClient = () => {
+  if (typeof window === 'undefined') {
+    // Server-side
+    return createServerClient(
+      process.env.SUPABASE_URL || '',
+      process.env.SUPABASE_ANON_KEY || '',
+      {
+        cookies
+      }
+    );
+  } else {
+    // Client-side
+    return createBrowserClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+    );
+  }
+};
