e369fb49ccd3de6945085bde8b84d3622db7e1cd
rateshsachdeva <rateshsachdeva@gmail.com>
2025-04-27 19:22:38 +0530

    Update SessionProviderWrapper.tsx

--- app/SessionProviderWrapper.tsx
+++ app/SessionProviderWrapper.tsx
@@ -1,8 +1,11 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
-import { ReactNode } from 'react';
 
-export default function SessionProviderWrapper({ children }: { children: ReactNode }) {
+export default function SessionProviderWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return <SessionProvider>{children}</SessionProvider>;
 }
